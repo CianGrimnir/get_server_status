c4ccb8349fe1f9820bf67cc44c12d89cb1b5cd3a
WolfgangMozart <rkhnair94@gmail.com>
2019-06-28 16:18:30 +0530

    Update Web_Monitor_JSON_Client.py

--- WebMonitor/Web_Monitor_JSON_Client.py
+++ WebMonitor/Web_Monitor_JSON_Client.py
@@ -5,11 +5,9 @@ import json
 import sys
 import time
 
-# if not len(sys.argv) > 1: sys.exit(1)
-# strategy=sys.argv[1]
 s = socket.socket()
-s.connect(('192.168.20.179', 68008))
-FILE = "/tmp/status1.json"
+s.connect(('192.168.SERVER.IP', 68008))
+FILE = "../status.json"
 while True:
     try:
         b = b''
@@ -29,7 +27,6 @@ while True:
         time.sleep(1)
     except ValueError:
         print "ValueError"
-        open('/tmp/ValueError','w').write(b)
     except KeyboardInterrupt:
         print "Socket", s
         if s:
