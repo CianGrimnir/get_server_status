e3362e9d02c93a3970d401197072416bc95c215c
WolfgangMozart <rkhnair94@gmail.com>
2019-06-28 16:13:39 +0530

    Update Variable_File.sh

--- Variable_File.sh
+++ Variable_File.sh
@@ -1,4 +1,4 @@
-source /home/mint/DAILY_CHECK/rakesh_script/checkall/variables.sh
+source rakesh_script/checkall/variables.sh
 
 for i in $stadd $cash;do echo "192.168.$i" ;done > hostfile1
 for i in $all_145 $future;do echo "192.168.$i" ;done > hostfile
